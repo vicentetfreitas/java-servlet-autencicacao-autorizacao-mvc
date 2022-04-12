539e4c2430a9d175bc84a175d7bb09c6281ee78a
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-04-11 21:49:19 -0300

    01-CriandoControlador

---
+++ gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -0,0 +1,25 @@
+package br.com.alura.gerenciador.acao;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
+
+public class ListaEmpresas {
+
+	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Listando empresas");
+
+		Banco banco = new Banco();
+		List<Empresa> empresas = banco.getEmpresas();
+		 request.setAttribute("empresas",empresas);
+		 RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+		 rd.forward(request, response);
+	}
+}
