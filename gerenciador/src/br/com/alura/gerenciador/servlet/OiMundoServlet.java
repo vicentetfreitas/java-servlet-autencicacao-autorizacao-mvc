a526fe7452b43a928b87e4cabf69026cc9fa178a
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-04-11 21:51:35 -0300

    01-Criando o Controlador

--- gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//oi
 //@WebServlet(urlPatterns="/oi")
 public class OiMundoServlet extends HttpServlet {
 	
