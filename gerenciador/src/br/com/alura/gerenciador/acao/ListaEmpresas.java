7ec584b065ffa591cf9280e16118879a8b2d0021
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-04-15 16:53:25 -0300

    05-Aplicando filtros.

--- gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -14,8 +14,7 @@ public class ListaEmpresas implements Acao {
 
 	public String executa(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-
+		
 		System.out.println("Listando empresas");
 
 		Banco banco = new Banco();
